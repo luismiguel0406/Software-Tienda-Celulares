3eb973d37db79567fff01165996105eb366d5947
Luis Del Orbe Martinez <luismigueldelorbe@hotmail.com>
2022-07-21 00:35:22 -0400

    icono a la aplicacion

--- MenuGerman/Properties/AssemblyInfo.cs
+++ MenuGerman/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("MenuGerman")]
+[assembly: AssemblyTitle("OnInventario")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MenuGerman")]
+[assembly: AssemblyProduct("OnInventario")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
